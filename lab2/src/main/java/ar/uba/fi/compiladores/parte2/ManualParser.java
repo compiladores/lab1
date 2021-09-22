09ae5f6144ada112e2b45f759331725038fb7998
jisbruzzi-velocity <jose.sbru@velocityautomotivesolutions.com>
2021-09-22 01:25:29 -0300

    copy labs repo

---
+++ lab2/src/main/java/ar/uba/fi/compiladores/parte2/ManualParser.java
@@ -0,0 +1,15 @@
+package ar.uba.fi.compiladores.parte2;
+
+import ar.uba.fi.compiladores.parte2.parser.ClosedLL1Machine;
+
+public class ManualParser {
+    public static void ex1(ClosedLL1Machine m){
+        //completar
+    }
+    public static void ex2(ClosedLL1Machine m){
+        //completar
+    }
+    public static void ex3(ClosedLL1Machine m){
+        //completar
+    }
+}
